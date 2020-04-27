d0aa9e61f86e5ba83d254f76b5af93f5dfafae9f
webturing <zj@webturing.com>
2020-04-27 09:23:40 +0800

    第13讲：数组和二分查找

---
+++ lec13array/JavaNew.java
@@ -0,0 +1,36 @@
+package lec13array;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class JavaNew {//jdk >=8.0 //jdk 11
+    public static void main(String[] args) {
+        List<Integer> a = Arrays.asList(new Integer[]{71, 12, 33, 44, 55, 66, 99, 100, 101});
+
+        a.forEach(System.out::println);//concise and elegant
+        //System.out.println(a[0])
+        //....
+        //System.out.println(a[a.length-1])
+
+        System.out.println();
+        a.stream().map(x -> x * 2).forEach(System.out::println);
+        System.out.println();
+
+        a.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("sum=" + a.stream().reduce(0, (x, y) -> (x + y)));
+        System.out.println("product=" + a.stream().limit(5).reduce(1, (x, y) -> (x * y)));
+        System.out.println("max=" + a.stream().reduce(a.get(0), Math::max));
+        System.out.println("min=" + a.stream().reduce(a.get(0), Math::min));
+
+        Collections.shuffle(a);
+        System.out.println(a);
+        Collections.sort(a, (x, y) -> x - y);//ascend
+        System.out.println(a);
+        Collections.sort(a, (x, y) -> y - x);//descend
+        System.out.println(a);
+        Collections.sort(a, (x, y) -> x % 10 - y % 10);//按照个位数排序
+        System.out.println(a);
+    }
+}
